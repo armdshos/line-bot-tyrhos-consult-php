c4b451239f4223803bc65027c580897a7a308edf
panu pakdeesan <arm.panu@gmail.com>
2020-04-08 15:44:21 +0700

    Update botpush_sticker.php

--- botpush_sticker.php
+++ botpush_sticker.php
@@ -1,5 +1,5 @@
 <?php
-$accessToken = "IKtQS7iWe72sF7u0/a6PpFVxuuiXnZ0Nc864Q8wd+U7dRaqpMjLeRXk/8A1QWQAHQLqPuTQj2XC8hWvjJDV5XMvtIlClUpSj0DPKuepgqJsWHwRFl9BHBrvxP6g9PVWnVJyMra6tTGOZK9wWrPP0rwdB04t89/1O/w1cDnyilFU=";
+$accessToken = "1ZpsWeHK7VrisXyxrFiozomC5j0EtQproAG3gvgB+P+kSRzDv2UmELxbF0yyIkofufGXB8KORv5qsL1jQzF6gP8iLyEw66gzvJe6Rk/Ur4dudD+INlPakngjTBK60V7A1CBCUk1ymMQrwh+a3mtQyAdB04t89/1O/w1cDnyilFU=";
    //$content = file_get_contents('php://input');
    //$arrayJson = json_decode($content, true);
    $arrayHeader = array();
@@ -8,15 +8,15 @@ $accessToken = "IKtQS7iWe72sF7u0/a6PpFVxuuiXnZ0Nc864Q8wd+U7dRaqpMjLeRXk/8A1QWQAH
    //รับข้อความจากผู้ใช้
    $message = 'สวัสดี';//$arrayJson['events'][0]['message']['text'];
    //รับ id ของผู้ใช้
-   $id = 'Ud4d744beed535713bb7f605ca0e1eba6';//$arrayJson['events'][0]['source']['userId'];
+   $id = 'Ub9af2ecddba401128eb33559023cbf9a';//$arrayJson['events'][0]['source']['userId'];
    #ตัวอย่าง Message Type "Text + Sticker"
    //if($message == "สวัสดี"){
       $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = "สวัสดีครับ สั่งสินค้าอะไรดี";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีครับ ";
       $arrayPostData['messages'][1]['type'] = "sticker";
       $arrayPostData['messages'][1]['packageId'] = "2";
-      $arrayPostData['messages'][1]['stickerId'] = "35";
+      $arrayPostData['messages'][1]['stickerId'] = "37";
       pushMsg($arrayHeader,$arrayPostData);
    //}
    function pushMsg($arrayHeader,$arrayPostData){
