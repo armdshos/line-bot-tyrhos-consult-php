68dad017d220669b7826ac107f92f4e885138478
panu pakdeesan <arm.panu@gmail.com>
2020-04-08 15:46:56 +0700

    Update botpush_image.php

--- botpush_image.php
+++ botpush_image.php
@@ -1,5 +1,5 @@
 <?php
-$accessToken = "IKtQS7iWe72sF7u0/a6PpFVxuuiXnZ0Nc864Q8wd+U7dRaqpMjLeRXk/8A1QWQAHQLqPuTQj2XC8hWvjJDV5XMvtIlClUpSj0DPKuepgqJsWHwRFl9BHBrvxP6g9PVWnVJyMra6tTGOZK9wWrPP0rwdB04t89/1O/w1cDnyilFU=";
+$accessToken = "1ZpsWeHK7VrisXyxrFiozomC5j0EtQproAG3gvgB+P+kSRzDv2UmELxbF0yyIkofufGXB8KORv5qsL1jQzF6gP8iLyEw66gzvJe6Rk/Ur4dudD+INlPakngjTBK60V7A1CBCUk1ymMQrwh+a3mtQyAdB04t89/1O/w1cDnyilFU=";
    //$content = file_get_contents('php://input');
    //$arrayJson = json_decode($content, true);
    $arrayHeader = array();
@@ -8,15 +8,15 @@ $accessToken = "IKtQS7iWe72sF7u0/a6PpFVxuuiXnZ0Nc864Q8wd+U7dRaqpMjLeRXk/8A1QWQAH
    //รับข้อความจากผู้ใช้
    $message = 'สวัสดี';//$arrayJson['events'][0]['message']['text'];
    //รับ id ของผู้ใช้
-   $id = 'Ud4d744beed535713bb7f605ca0e1eba6';//$arrayJson['events'][0]['source']['userId'];
+   $id = 'Ub9af2ecddba401128eb33559023cbf9a';//$arrayJson['events'][0]['source']['userId'];
    #ตัวอย่าง Message Type "Text + Sticker"
    //if($message == "สวัสดี"){
       $arrayPostData['to'] = $id;
       $arrayPostData['messages'][0]['type'] = "text";
       $arrayPostData['messages'][0]['text'] = "รูปโลโก้";
       $arrayPostData['messages'][1]['type'] = "image";
-      $arrayPostData['messages'][1]['originalContentUrl'] = "https://youant.net/green/images/48412313_558355137924389_6011670775092740096_n.jpg";
-      $arrayPostData['messages'][1]['previewImageUrl'] = "https://youant.net/green/images/48412313_558355137924389_6011670775092740096_n.jpg";
+      $arrayPostData['messages'][1]['originalContentUrl'] = "https://www.youant.net/wp-content/uploads/2020/04/logo-new-128.png";
+      $arrayPostData['messages'][1]['previewImageUrl'] = "https://www.youant.net/wp-content/uploads/2020/04/logo-new-128.png";
       pushMsg($arrayHeader,$arrayPostData);
    //}
    function pushMsg($arrayHeader,$arrayPostData){
