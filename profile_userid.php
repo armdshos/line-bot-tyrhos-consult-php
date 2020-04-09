0f41bfa26ab7c799de70b487f7bf9c8396a22f4c
panu pakdeesan <arm.panu@gmail.com>
2020-04-09 15:52:34 +0700

    Update profile_userid.php

--- profile_userid.php
+++ profile_userid.php
@@ -4,10 +4,11 @@ $datas = file_get_contents('php://input');
 $deCode = json_decode($datas,true);
 $replyuserId = $deCode['events'][0]['userId'];
 echo $replyuserId;
+echo "<hr>";
 
 $access_token = '1ZpsWeHK7VrisXyxrFiozomC5j0EtQproAG3gvgB+P+kSRzDv2UmELxbF0yyIkofufGXB8KORv5qsL1jQzF6gP8iLyEw66gzvJe6Rk/Ur4dudD+INlPakngjTBK60V7A1CBCUk1ymMQrwh+a3mtQyAdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Ub9af2ecddba401128eb33559023cbf9a';
+$userId = $replyuserId;//'Ub9af2ecddba401128eb33559023cbf9a';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
