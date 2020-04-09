73d5e71eb7a01e7ddcfe88d9ec0f18f5df989b16
panu pakdeesan <arm.panu@gmail.com>
2020-04-09 15:38:23 +0700

    Create profile_userid.php

---
+++ profile_userid.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$access_token = '1ZpsWeHK7VrisXyxrFiozomC5j0EtQproAG3gvgB+P+kSRzDv2UmELxbF0yyIkofufGXB8KORv5qsL1jQzF6gP8iLyEw66gzvJe6Rk/Ur4dudD+INlPakngjTBK60V7A1CBCUk1ymMQrwh+a3mtQyAdB04t89/1O/w1cDnyilFU=';
+
+$userId = 'Ub9af2ecddba401128eb33559023cbf9a';
+
+$url = 'https://api.line.me/v2/bot/profile/'.$userId;
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
