4621327a477edd0da6225cadff52883b544407a7
panu pakdeesan <arm.panu@gmail.com>
2020-04-09 16:08:11 +0700

    Update profile_userid.php

--- profile_userid.php
+++ profile_userid.php
@@ -1,9 +1,15 @@
 <?php
 
+/*Get Data From POST Http Request*/
 $datas = file_get_contents('php://input');
+/*Decode Json From LINE Data Body*/
 $deCode = json_decode($datas,true);
-$replyuserId = $deCode['events'][0]['userId'];
-echo "userid = ".$replyuserId;
+
+file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+
+$replyToken = $deCode['events'][0]['replyToken'];
+//$replyuserId = $deCode['events'][0]['userId'];
+echo "Token = ".$replyToken;
 //echo "<hr>";
 
 $access_token = '1ZpsWeHK7VrisXyxrFiozomC5j0EtQproAG3gvgB+P+kSRzDv2UmELxbF0yyIkofufGXB8KORv5qsL1jQzF6gP8iLyEw66gzvJe6Rk/Ur4dudD+INlPakngjTBK60V7A1CBCUk1ymMQrwh+a3mtQyAdB04t89/1O/w1cDnyilFU=';
